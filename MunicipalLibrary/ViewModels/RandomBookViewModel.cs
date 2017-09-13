e94106ef920c7da6d09ea12e4bc9bc01e320f762
FranciellyAlegre <FranciellyAlegre@DESKTOP-SJ1FMQ6>
2017-09-12 21:33:01 -0300

    Mudanças

--- MunicipalLibrary/ViewModels/RandomBookViewModel.cs
+++ MunicipalLibrary/ViewModels/RandomBookViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using MunicipalLibrary.Models;
 
-namespace Vidly.ViewModels
+namespace MunicipalLibrary.ViewModels
 {
     public class RandomBookViewModel
     {
